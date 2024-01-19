3f7eea042de3560d6dee9c761be3240a284548fe
herofab <mailstoumer@gmail.com>
2024-01-19 22:22:08 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var newsletter = document.querySelector(".newsletter");
+var closeButton = document.querySelector(".newsletter .close");
+
+closeButton.onclick = function() {
+ newsletter.style.display = "none";
+}
